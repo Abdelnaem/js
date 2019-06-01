bec3cfd6c98d2b52ae0bbf133b34f1d4ca75209b
Abdelnaem <30398710+Abdelnaem@users.noreply.github.com>
2019-06-01 03:25:34 +0200

    Update and rename obj to test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+{
+  firstname: "Abdelnaem",
+  lastname: "Atia",
+  from: "egypt",
+  age: 22
+}
+
+var myName = "Abdelnaem Atia", myAge = 22, myCountry = "Egypt", bodyTag = document.body, randomNumber = Math.random();
