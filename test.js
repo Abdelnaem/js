8ad003e694e447fb74b70168b3d8dc5001db5c34
Abdelnaem <30398710+Abdelnaem@users.noreply.github.com>
2019-06-01 03:27:58 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-{
-  firstname: "Abdelnaem",
-  lastname: "Atia",
-  from: "egypt",
-  age: 22
-}
-
 var myName = "Abdelnaem Atia", myAge = 22, myCountry = "Egypt", bodyTag = document.body, randomNumber = Math.random();
